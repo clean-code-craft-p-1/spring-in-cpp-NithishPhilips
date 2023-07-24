279cccf782f10ce825c3d1a84822b638e3c68f80
NithishPhilips <nithishkumar.punna@philips.com>
2023-07-24 19:31:38 +0530

    First

--- stats.cpp
+++ stats.cpp
@@ -1,8 +1,56 @@
+#include "pch.h"
 #include "stats.h"
+#include <math.h>
+#include <iostream>
+#include <algorithm>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<double>& vect) {
     //Implement statistics here
-	
-	
-	gahsdha
+
+	Stats stat;
+
+	if (vect.size() == 0)
+	{
+		return stat;
+	}
+	else
+	{
+		stat.max = *max_element(vect.begin(), vect.end());
+		stat.min = *min_element(vect.begin(), vect.end());
+
+		double sum = 0.0;
+		for (const float &i : vect) {
+			sum += (double)i;
+		}
+		stat.average = (float) sum / vect.size();
+	}
+
+	return stat;
 }
+
+void EmailAlert::Alert()
+{
+	
+}
+
+void LEDAlert::Alert()
+{
+
+}
+
+void StatsAlerter::checkAndAlert(const std::vector<double>& vec)
+{
+	EmailAlert email;
+	LEDAlert led;
+
+	for (double a : vec)
+	{
+		if (a > 10.8)
+		{
+			email.emailSent = true;
+			led.ledGlows = true;
+		}
+	}
+}
+
+
--- stats.h
+++ stats.h
@@ -1,7 +1,42 @@
 #include <vector>
 
+class IAlerter {
+	virtual void Alert() = 0;
+};
+
+class EmailAlert : public IAlerter {
+public:
+	bool emailSent;
+	void Alert();
+};
+
+class LEDAlert : public IAlerter {
+public:
+	bool ledGlows;
+	void Alert();
+};
+
+class StatsAlerter {
+public:
+	StatsAlerter(const float, std::vector<IAlerter*>);
+
+	void checkAndAlert(const std::vector<double>&);
+};
+
 namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
-    
-    Stats ComputeStatistics(const std::vector<___>& );
+
+	struct Stats {
+	public:
+		double average = 0;
+		double max = 0;
+		double min = 0;
+
+	};
+
+    Stats ComputeStatistics(const std::vector<double>& );
+
 }
+
+
+
